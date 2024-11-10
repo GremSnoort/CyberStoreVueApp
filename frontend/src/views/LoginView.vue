4357531e197757277c1b112d67eaf560768d5b43
GremSnoort <grem_snoort@protonmail.com>
2024-11-10 16:40:58 +0300

    LoginView Style

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,13 +1,20 @@
 <template>
-<div>
-    <form @submit.prevent="authenticate()" class="login-form">
-        <label for="">E-mail:</label>
-        <input v-model="auth.email" type="email">
+<div class="login container container-flex-column">
+    <form @submit.prevent="authenticate()" class="login-form container-flex-column">
+        <img src="https://cdn-icons-png.flaticon.com/512/6681/6681204.png" alt="Login" class="login-form-item container-flex-row" style="width: 12%;">
+        <div class="login-form-item container-flex-row">
+            <label for="">E-mail</label>
+            <input v-model="auth.email" type="email">
+        </div>
 
-        <label for="">Password</label>
-        <input v-model="auth.password" type="password">
+        <div class="login-form-item container-flex-row">
+            <label for="">Password</label>
+            <input v-model="auth.password" type="password">
+        </div>
 
-        <button type="submit">Login</button>
+        <div class="login-form-item container-flex-row">
+            <button type="submit">Login</button>
+        </div>
     </form>
 </div>
 </template>
@@ -67,5 +74,49 @@ export default {
 <style scoped>
 .login-form {
     width: 100%;
+}
+.login-form-item{
+    width: 30%;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+}
+.login-form-item label {
+    font-family: "ABeeZee", sans-serif;
+    font-weight: 400;
+    font-style: bold;
+    width: 25%;
+}
+.login-form-item input {
+    border: none;
+    outline: none;
+    width: 70%;
+    height: 2rem;
+    border-radius: 5%;
+    opacity: 0px;
+    background: #F5F5F5;
+    font-family: "ABeeZee", sans-serif;
+    font-weight: 400;
+    font-style: italic;
+    font-size: 14px;
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+.login-form-item button {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    width: 100%;
+    border-radius: 2%;
+    font-family: "ABeeZee", sans-serif;
+    font-weight: 400;
+    font-style: bold;
+    font-size: 32px;
+    text-align: center;
+}
+.login-form-item button:hover {
+    background-color:cadetblue;
+}
+
+.login-form-item input:hover {
+    background-color: cadetblue;
 }
 </style>
